f47517c465902d47612987bf9ffe36ebc4a25e9f
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-08-07 22:18:37 -0300

    Desafio01+RuedaSanchez (act)

--- challenge-01/challenge-01.js
+++ challenge-01/challenge-01.js
@@ -17,14 +17,14 @@ class ProductManager{
         }
 
         if(newProduct.title == null || newProduct.description == null || newProduct.price == null || newProduct.thumbnail == null || newProduct.code == null || newProduct.stock == null){
-            console.log("Error. All product fields must be complete!");
+            return console.log("Error. All product fields must be complete!");
         }
         
         const productWithSameCode = this.products.filter((prod) => prod.code === newProduct.code); // retorna un arreglo con los objetos (productos) que coincidan con el codigo del producto que se desea agregar
         
         if(productWithSameCode.length > 0){
             // se repitieron los codigos en distintos productos
-            console.log("Error. Cannot be different products with the same code!"); 
+            return console.log("Error. Cannot be different products with the same code!"); 
         }
         
         if(this.products.length === 0){
@@ -33,7 +33,7 @@ class ProductManager{
             newProduct.id = this.products.length + 1;
         }
         
-        this.products.push(newProduct);
+        return this.products.push(newProduct);
     }
     
     getProducts(){
