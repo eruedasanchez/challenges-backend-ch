c9d5c2000aeda88b1f1ce0593bc6d1a957aab012
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-09-18 16:00:56 -0300

    Preentrega-01 corrections in cart router for integrative practice

--- preentrega-01/src/routes/carts.router.js
+++ preentrega-01/src/routes/carts.router.js
@@ -12,70 +12,104 @@ const getCart = () => {
     return JSON.parse(fs.readFileSync(route, 'utf-8'));
 }
 
-const saveCart = cart => {
+const save = cart => {
     fs.writeFileSync(route, JSON.stringify(cart, null, '\t'));
 }
 
-router.post('/', (req,res) => {
-    let { products } = req.body;
+/*------------------------------*\
+    #MIDDLEWARES GET '/:cid'
+\*------------------------------*/
 
-    if(!products){
-        return res.status(400).json({error:'El campo products es obligatorio!'});
-    }
-
-    let cart = getCart();
-    
-    let cartId = 1;
-    if(cart.length > 0) cartId = cart[cart.length-1].orderId + 1;
-
-    let newOrder = {
-        orderId: cartId, 
-        products: products 
-    }
-
-    for(const prod of products){
-        // Se chequea que por el body se pase el formato pedido para cada producto
-        if(!(Object.keys(prod).includes("productId") && Object.keys(prod).includes("quantity") && Object.keys(prod).length === 2)){
-            return res.status(400).json({error:'Se debe pasar el siguiente formato por el body: {"products":[{"productId":number, "quantity":number}]}'});
-        }  
-    }
-    
-    cart.push(newOrder);
-
-    saveCart(cart);
-
-    res.status(200).json(newOrder);
-})
-
-router.get('/:cid', (req, res) => {
-    let cart = getCart();
+const nanMid = (req, res, next) => {
     let cid = parseInt(req.params.cid);
     
     if(isNaN(cid)) return res.status(400).json({status:'error', message:'Requiere un argumento cid de tipo numerico'});
-    
-    let orderSelected = cart.filter(order => order.orderId === cid);
-    if(orderSelected.length > 0){
-        return res.status(200).json({status:'ok', orderDetails: orderSelected[0].products});                              // Caso en el que se cumple http://localhost:8080/api/carts/2
-    } else {
-        return res.status(400).json({status:'error', message:`La orden con CID ${cid} no existe`}); // Caso en el que se cumple http://localhost:8080/api/carts/34123123
-    }
-})
 
-router.post('/:cid/product/:pid', (req,res) => {
+    next();
+}
+
+const invalidPidMid = (req, res, next) => {
     let cart = getCart();
+    let cid = parseInt(req.params.cid);
+    
+    let cartSelected = cart.filter(cart => cart.cartId === cid);
+    if(cartSelected.length === 0){
+        return res.status(400).json({status:'error', message:`El carrito con CID ${cid} no existe`}); // Caso en el que se cumple http://localhost:8080/api/carts/34123123
+    }
+
+    next();
+}
+
+/*------------------------------------------*\
+    #MIDDLEWARES POST '/:cid/product/:pid'
+\*------------------------------------------*/
+
+const nanCidPid = (req, res, next) => {
     let cid = parseInt(req.params.cid);
     let pid = parseInt(req.params.pid);
     
     if(isNaN(cid) || isNaN(pid)) return res.status(400).json({status:'error', message:'Se requieren argumentos cid y pid de tipo numerico'});
 
+    next();
+}
+
+const pidInvalid = (req, res, next) => {
+    let pid = parseInt(req.params.pid);
+
     if(pid < 1) return res.status(400).json({status:'error', message:`El PID ${pid} es invalido. Deben ser mayores o iguales a 1`});
+
+    next();
+}
+
+const invalidCidMid = (req, res, next) => {
+    let cart = getCart();
+    let cid = parseInt(req.params.cid);
     
-    let idxOrder = cart.findIndex(order => order.orderId === cid);
-    if(idxOrder === -1) return res.status(400).json({error:`La orden con CID ${cid} no existe`});
+    let idxCart = cart.findIndex(cart => cart.cartId === cid);
+    if(idxCart === -1) return res.status(400).json({error:`El carrito con CID ${cid} no existe`});
 
-    let orderProducts = cart[idxOrder].products;
+    next();
+}
 
-    let idxPid = orderProducts.findIndex(prod => prod.productId === pid);
+/*------------------------------*\
+        #CART ROUTES
+\*------------------------------*/
+
+router.post('/', (req,res) => {
+    let cart = getCart();
+    
+    let carritoId = 1;
+    if(cart.length > 0) carritoId = cart[cart.length-1].cartId + 1;
+
+    let newCart = {
+        cartId: carritoId, 
+        products: [] 
+    }
+    
+    cart.push(newCart);
+
+    save(cart);
+
+    res.status(200).json(newCart);
+})
+
+router.get('/:cid', nanMid, invalidPidMid, (req, res) => {
+    let cart = getCart();
+    let cid = parseInt(req.params.cid);
+    
+    let cartSelected = cart.filter(cart => cart.cartId === cid);
+    return res.status(200).json({status:'ok', cartProducts: cartSelected[0].products});                              // Caso en el que se cumple http://localhost:8080/api/carts/2
+})
+
+router.post('/:cid/product/:pid', nanCidPid, pidInvalid, invalidCidMid, (req,res) => {
+    let cart = getCart();
+    let cid = parseInt(req.params.cid);
+    let pid = parseInt(req.params.pid);
+    
+    let idxCart = cart.findIndex(cart => cart.cartId === cid);
+    let productsSelected = cart[idxCart].products;
+
+    let idxPid = productsSelected.findIndex(prod => prod.productId === pid);
     if(idxPid === -1){
         // El producto no se encuentra definido en esta orden de compra.
         // Se agrega de a una unidad como solicita el enunciado
@@ -83,14 +117,14 @@ router.post('/:cid/product/:pid', (req,res) => {
             productId: pid,
             quantity: 1 
         }
-        orderProducts.push(newProductAdded);
-        cart[idxOrder].products = orderProducts; 
+        productsSelected.push(newProductAdded);
+        cart[idxCart].products = productsSelected; 
     } else {
         // Se aumenta en uno la cantidad del producto como solicita el enunciado
-        orderProducts[idxPid].quantity += 1;
+        productsSelected[idxPid].quantity += 1;
     }
 
-    saveCart(cart);
+    save(cart);
 
     res.status(200).json({cart});
 })
