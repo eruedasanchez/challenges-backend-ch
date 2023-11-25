19e389d4fe9a569564bd34b5b1a9d3c7c3709c3c
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-11-25 18:54:37 -0300

    Challenge-09+RuedaSanchez

--- challenge-09/src/app.js
+++ challenge-09/src/app.js
@@ -81,6 +81,6 @@ try {
     logger.fatal(`Error al conectarse con MongoDB Atlas. Detalle: ${error.message}`);
 }
 
-// Ultimo commit Challenge-08+RuedaSanchez 
+// Ultimo commit Challenge-09+RuedaSanchez 
 
 
