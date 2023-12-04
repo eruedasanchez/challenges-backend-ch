46bbcaea0a3362a7998aba40a85757e052abc549
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-12-03 22:07:07 -0300

    Depuring code

--- integrative-practice-03/src/app.js
+++ integrative-practice-03/src/app.js
@@ -7,8 +7,6 @@ import session from 'express-session';
 import ConnectMongo from 'connect-mongo';
 import passport from 'passport';
 import cookieParser from 'cookie-parser';
-// import nodemailer from 'nodemailer';
-// import multer from 'multer';
 import {Server} from 'socket.io';
 import {router as productsRouter} from './routes/products.router.js';
 import {router as cartsRouter} from './routes/carts.router.js';
@@ -23,44 +21,8 @@ import { middLog, logger } from './utilsWinston.js';
 
 const PORT = config.PORT;
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, './src/tmp/uploads')
-//     },
-//     filename: function (req, file, cb) {
-//         // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//         cb(null, file.originalname)
-//     }
-// })
-
-// const upload = multer({ storage: storage })
-
 const PERSISTENCE = config.PERSISTENCE;
 logger.info(`Persistencia en ${PERSISTENCE} iniciada`);
-
-
-
-// const enviar = async (to, subject, message, adjuntos=[]) => {
-//     let attachments = [];
-
-//     adjuntos.forEach(adjunto => {
-//         attachments.push({
-//             path: adjunto.path,
-//             filename: adjunto.filename
-//         })
-//     })
-
-//     return transporter.sendMail({
-//         from: 'Ezequiel <ezequiel.ruedasanchez@gmail.com>',
-//         to: to,
-//         subject: subject,
-//         html: `
-//         <p style="color:blue; background-color:red">${message}</p>
-//         <img src="cid:adjunto01">`,
-//         // agregamos archivos adjuntos
-//         attachments: attachments
-//     });
-// }
 
 const app = express();
 
@@ -99,21 +61,6 @@ app.use(passport.initialize());
 // 3. cargar el middleware de passport en sessions router
 
 app.use('/api/sessions', sessionsRouter);
-
-// app.post('/mail', upload.array("adjuntos"), async (req,res) =>  {
-//     let {to, subject, message} = req.body;
-
-    // console.log("req.file", req.file);
-    // console.log("req.files", req.files);
-
-//     let resultado = await enviar(to, subject, message, req.files);
-
-
-//     res.setHeader('Content-Type', 'text/plain');
-//     return res.status(200).json({payload: resultado});
-
-// })
-
 app.use('/api/users', usersRouter);
 app.use('/', viewsRouter);
 
@@ -139,40 +86,4 @@ try {
 }
 
 // Ultimo commit Challenge-09+RuedaSanchez 
-
-// import nodemailer from 'nodemailer';
-
-// const transporter = nodemailer.createTransport({
-//     // transporte valido solo para gmail por eso utilizamos serivce y el puerto 587
-//     service: 'gmail',
-//     port: '587',
-//     auth: {
-//         user: 'ezequiel.ruedasanchez@gmail.com',
-//         pass: 'szmiutoluzhlmeps'
-//     }
-// })
-
-// const enviar = async () => {
-//     return transporter.sendMail({
-//         from: 'Ezequiel <ezequiel.ruedasanchez@gmail.com>',
-//         to: 'ezequiel.ruedasanchez@gmail.com',
-//         subject: 'prueba email con adjuntos ahora',
-//         html: `
-//         <p style="color:blue; background-color:red">Prueba de email simple</p>
-//         <img src="cid:adjunto01">`,
-//         // agregamos archivos adjuntos
-//         attachments: [
-//             {
-//                 path: path.join(__dirname, "public/assets/img/diego10.jpg"),
-//                 filename: "diego10.jpg",
-//                 cid: 'adjunto01'
-//             }
-//         ]
-//     });
-// }
-
-// enviar()
-//     .then(result => console.log(result))
-//     .catch(error => console.log(error))
-
 
